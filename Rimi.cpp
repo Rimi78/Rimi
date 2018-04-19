7920ade19a4174f2153f006d50d34d854c5d097a
Student <ummehani.rimi74@gmail.com>
2018-04-19 13:39:31 +0600

    created hello.c file

--- Rimi.cpp
+++ Rimi.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 using namespace std;
+int mult(int a, int b)
+{
+    return a*b;
+}
 int add( int a, int b)
 {
     return a+b;
@@ -8,6 +12,7 @@ int main()
 {
     int rimi= add(2,4);
     cout<<rimi<<endl;
-    int prity= add(10,30);
+
+    int prity= mult(2,4);
     cout<<prity<<endl;
 }
